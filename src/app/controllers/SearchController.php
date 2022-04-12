37f9facab571f23e3c11d76e63485b02497e34cd
Madhurjai <madhurjaiswal@cedcoss.com>
2022-04-12 18:17:12 +0530

    final commit

--- src/app/controllers/SearchController.php
+++ src/app/controllers/SearchController.php
@@ -20,6 +20,13 @@ class SearchController extends Controller
                 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization: Bearer ' . $this->session->get('token')));
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                 $val = curl_exec($ch);
+                if (curl_exec($ch) !== 'false') {
+                    $this->EventManager->fire('tokens:updatetoken', $this);
+                    $ch = curl_init($url);
+                    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization: Bearer ' . $this->session->get('token')));
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    $val = curl_exec($ch);
+                }
                 $v = json_decode($val);
                 $this->view->album = $v->albums->items;
             }
